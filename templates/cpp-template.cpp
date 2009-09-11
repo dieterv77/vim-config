a9283d473052899d9b23b69ea9ec582d1773e25f
dvandenbussche <dvandenbussche@69741975-fc24-0410-a6ce-a1e7f53f4316>
2009-09-11 13:26:31 +0000

    Add cpp template
    git-svn-id: svn+ssh://tafelbier/home/dvandenbussche/Windows-Home/svn/VimConfig@653 69741975-fc24-0410-a6ce-a1e7f53f4316

---
+++ templates/cpp-template.cpp
@@ -0,0 +1,12 @@
+/* $Id: dvandenbussche $
+ *
+ * Copyright (c) 2009 Axioma, Inc.
+ */
+#include <iostream>
+
+using namespace std;
+
+void main(int argc, char *argv[])
+{
+  cout << "Hello World!" << endl;   
+}
