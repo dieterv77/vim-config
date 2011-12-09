10a9363dc14d4ac9c10829a40000d16bbace4d97
dvandenbussche <dvandenbussche@69741975-fc24-0410-a6ce-a1e7f53f4316>
2011-12-09 18:26:27 +0000

    Reindent and variable rename
    git-svn-id: svn+ssh://tafelbier/home/dvandenbussche/Windows-Home/svn/VimConfig@726 69741975-fc24-0410-a6ce-a1e7f53f4316

--- templates/python-template.py
+++ templates/python-template.py
@@ -4,14 +4,14 @@ import sys
 from optparse import OptionParser
 
 def runmain(argv=None):
-   if argv == None:
-      argv = sys.argv
-   
-   usage = 'usage: %prog [options]\n'
-   parser = OptionParser(usage=usage)
-   parser.add_option("-f", "--file", dest="filename",
-                           help="Read solution from FILE", metavar="FILE")
-   (options, args) = parser.parse_args(argv)
+    if argv == None:
+        argv = sys.argv
+
+    usage = 'usage: %prog [options]\n'
+    parser = OptionParser(usage=usage)
+    parser.add_option("-f", "--file", dest="filename",
+                            help="Read solution from FILE", metavar="FILE")
+    (cmdoptions, args) = parser.parse_args(argv)
 
 if __name__ == "__main__":
-   runmain()
+    runmain()
